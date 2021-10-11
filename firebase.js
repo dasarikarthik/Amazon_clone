d4820ec13531c681bb8dae97fe915745253c446a
dasarikarthik <75360723+dasarikarthik@users.noreply.github.com>
2021-10-11 15:10:32 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,12 +1,7 @@
 import firebase from "firebase"; 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDyG3svUBTTKYqzGOV3C_Zew57GzG9bUx8",
-    authDomain: "clone-488b8.firebaseapp.com",
-    projectId: "clone-488b8",
-    storageBucket: "clone-488b8.appspot.com",
-    messagingSenderId: "1073536965377",
-    appId: "1:1073536965377:web:1c6719d2a732f2826dd900"
+   //Enter your details
   };
   const app=!firebase.apps.length ? firebase.initializeApp(firebaseConfig): firebase.app();
   const db=app.firestore();
